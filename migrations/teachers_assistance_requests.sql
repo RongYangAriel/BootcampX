138c14deb98357fb8853230be3f86bc070d028d3
RongYangAriel <yang.ariel@hotmail.com>
2020-11-24 15:29:09 -0500

    add assistance_requests & teachers TABLE

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -6,14 +6,14 @@ CREATE TABLE teachers (
   is_active BOOLEAN DEFAULT TRUE
 );
 
-CREATE TABLE assistance_request (
+CREATE TABLE assistance_requests (
   id SERIAL PRIMARY KEY NOT NULL,
-  assignment_id  REFERENCES assignments(id) ON DELETE CASCADE,
-  student_id REFERENCES students(id)  ON DELETE CASCADE,
-  teacher_id  REFERENCES teachers(id)   ON DELETE CASCADE,
-  created_at TIMESTAMP
-  started_at TIMESTAMP
-  completed_at  TIMESTAMP
-  student_feedback TEXT
-  teacher_feedback  TEXT
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  created_at TIMESTAMP,
+  started_at TIMESTAMP,
+  completed_at TIMESTAMP,
+  student_feedback TEXT,
+  teacher_feedback TEXT
 );
